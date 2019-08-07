ed33a9ea3201d9e346ec4fdd26c976c5a1ae0433
NaomiOkiddy <naomi.okiddy@gmail.com>
2019-08-07 20:04:48 +0300

    Adding 42 projects

---
+++ 42/get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nokiddy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/21 09:16:26 by nokiddy           #+#    #+#             */
+/*   Updated: 2018/10/21 09:16:27 by nokiddy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 1024
+# define NULL_CHECK(x) if (x == NULL) return (-1);
+
+int get_next_line(const int fd, char **line);
+
+#endif
